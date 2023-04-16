f18d40fdc59e2284768aa8594eb304118cbb75b9
Emilio <emilio.ybarra@outlook.com>
2023-04-16 22:11:58 +0200

    Remove tutorial component and added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <main :class="[$route.name]" class="main px-4 md:px-0">
+    <div class="container mx-auto">
+      <nuxt class="nuxt" />
+    </div>
+
+    <svg class="divider -mx-4 sm:mx-0" width="100%" viewBox="0 0 10 1">
+      <polygon stroke="none" stroke-width="0" fill="#0b3765" points="0,0.6 0,1 10,1 10,0" />
+    </svg>
+
+
+  </main>
+</template>
+
+<style lang="scss">
+.divider {
+  position: relative;
+  min-width: 100vw;
+  margin-top: auto;
+}
+
+.nuxt {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
-  <Tutorial />
+  <h1>Hello World</h1>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage',
-})
 </script>
